24ddc30bc56a6c9339f2f67b71b7cace3090ecd1
Oleg <ov.chilikhin2022@yandex.ru>
2023-01-12 20:39:19 +0400

    Add WebPack config

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+	context: path.resolve(__dirname, "src"),
+	mode: "development",
+	entry: "./index.js",
+	output: {
+		filename: "bundle.[hash].js",
+		path: path.resolve(__dirname, "dist"),
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new HTMLWebpackPlugin({
+			template: "index.html",
+		}),
+		new CopyPlugin({
+			patterns: [
+				{
+					from: path.resolve(__dirname, "src/favicon.ico"),
+					to: path.resolve(__dirname, "dist"),
+				},
+			],
+		}),
+	],
+};
