fe3820fd0c057762a8b0aca92f172723e0e3347e
Oleg <ov.chilikhin2022@yandex.ru>
2023-01-22 19:38:59 +0400

    Add Linter and Formatters

--- src/index.js
+++ src/index.js
@@ -2,35 +2,35 @@ import modalSelect from "./components/modalSelect.pug";
 import Game from "./components/GameScreen.pug";
 
 window.application = {
-	difficult: "",
-	timer: {
-		minutes: 0,
-		seconds: 0,
-	},
-	cards: [],
-	userCards: [],
+    difficult: "",
+    timer: {
+        minutes: 0,
+        seconds: 0,
+    },
+    cards: [],
+    userCards: [],
 };
 
 window.addEventListener("DOMContentLoaded", () => {
-	const app = document.querySelector("#app");
-	app.innerHTML = modalSelect();
+    const app = document.querySelector("#app");
+    app.innerHTML = modalSelect();
 
-	const btnStart = document.querySelector(".btn__start");
-	const radios = document.querySelectorAll(".choice__input");
-	radios.forEach((radio) => {
-		radio.addEventListener("change", () => {
-			window.application.difficult = radio.value;
-		});
-	});
-	btnStart.addEventListener("click", (e) => {
-		e.preventDefault();
-		if (window.application.difficult === "") {
-			alert("Select the Difficult");
-			return;
-		}
-		const GameScreen = Game({
-			difficult: window.application.difficult,
-		});
-		app.innerHTML = GameScreen;
-	});
+    const btnStart = document.querySelector(".btn__start");
+    const radios = document.querySelectorAll(".choice__input");
+    radios.forEach((radio) => {
+        radio.addEventListener("change", () => {
+            window.application.difficult = radio.value;
+        });
+    });
+    btnStart.addEventListener("click", (e) => {
+        e.preventDefault();
+        if (window.application.difficult === "") {
+            alert("Выберите сложность");
+            return;
+        }
+        const GameScreen = Game({
+            difficult: window.application.difficult,
+        });
+        app.innerHTML = GameScreen;
+    });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -5,78 +5,73 @@ const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
-	entry: {
-		// define all Pug files here
-		index: "./src/index.pug",
-	},
-
-	output: {
-		path: path.join(__dirname, "./dist/"),
-		publicPath: isDev ? "/" : "./",
-	},
-
-	resolve: {
-		alias: {
-			// use alias to avoid relative paths like `./../../images/`
-			Images: path.join(__dirname, "./src/images/"),
-			Fonts: path.join(__dirname, "./src/fonts/"),
-			Sass: path.join(__dirname, "./src/sass/"),
-		},
-	},
-
-	plugins: [
-		new CleanWebpackPlugin(),
-		new PugPlugin({
-			js: {
-				// output filename of extracted JS file from source script
-				filename: "assets/js/[name].[contenthash:8].js",
-			},
-			css: {
-				// output filename of extracted CSS file from source style
-				filename: "assets/css/[name].[contenthash:8].css",
-			},
-		}),
-	],
-
-	module: {
-		rules: [
-			{
-				test: /\.pug$/,
-				oneOf: [
-					// import Pug in JavaScript/TypeScript as template function
-					{
-						issuer: /\.(js|ts)$/, // match scripts where Pug is used
-						loader: PugPlugin.loader,
-						options: {
-							method: "compile", // compile Pug into template function
-						},
-					},
-					// render Pug from Webpack entry into static HTML
-					{
-						loader: PugPlugin.loader, // default method is 'render'
-					},
-				],
-			},
-			{
-				test: /\.(css|sass|scss)$/,
-				use: ["css-loader", "sass-loader"],
-			},
-			{
-				test: /\.(png|jpg|jpeg|ico)/,
-				type: "asset/resource",
-				generator: {
-					// output filename of images
-					filename: "assets/img/[name].[hash:8][ext]",
-				},
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
-				type: "asset/resource",
-				generator: {
-					// output filename of fonts
-					filename: "assets/fonts/[name][ext][query]",
-				},
-			},
-		],
-	},
+    entry: {
+        index: "./src/index.pug",
+    },
+    output: {
+        path: path.join(__dirname, "./dist/"),
+        publicPath: isDev ? "/" : "./",
+    },
+    resolve: {
+        alias: {
+            // use alias to avoid relative paths like `./../../images/`
+            Images: path.join(__dirname, "./src/images/"),
+            Fonts: path.join(__dirname, "./src/fonts/"),
+            Sass: path.join(__dirname, "./src/sass/"),
+        },
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new PugPlugin({
+            js: {
+                // output filename of extracted JS file from source script
+                filename: "assets/js/[name].[contenthash:8].js",
+            },
+            css: {
+                // output filename of extracted CSS file from source style
+                filename: "assets/css/[name].[contenthash:8].css",
+            },
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.pug$/,
+                oneOf: [
+                    // import Pug in JavaScript/TypeScript as template function
+                    {
+                        issuer: /\.(js|ts)$/, // match scripts where Pug is used
+                        loader: PugPlugin.loader,
+                        options: {
+                            method: "compile", // compile Pug into template function
+                        },
+                    },
+                    // render Pug from Webpack entry into static HTML
+                    {
+                        loader: PugPlugin.loader, // default method is 'render'
+                    },
+                ],
+            },
+            {
+                test: /\.(css|sass|scss)$/,
+                use: ["css-loader", "sass-loader"],
+            },
+            {
+                test: /\.(png|jpg|jpeg|ico)/,
+                type: "asset/resource",
+                generator: {
+                    // output filename of images
+                    filename: "assets/img/[name].[hash:8][ext]",
+                },
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf|svg)$/i,
+                type: "asset/resource",
+                generator: {
+                    // output filename of fonts
+                    filename: "assets/fonts/[name][ext][query]",
+                },
+            },
+        ],
+    },
 };
